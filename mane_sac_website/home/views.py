f6a8a4a4539e40438f0e1447dd39e9c95782fe97
sohan-4 <101779634+rgc14@users.noreply.github.com>
2024-09-27 16:39:40 -0400

    Added basic pages with placeholder information to github

---
+++ mane_sac_website/home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home/home.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def members(request):
+    return render(request, 'home/members.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
